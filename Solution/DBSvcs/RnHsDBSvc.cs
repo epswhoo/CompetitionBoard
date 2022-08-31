51e8bf95841b0cb5ddcd8123cdbdf6e2c97598de
Mona Parnet <mona@parnet.de>
2022-08-31 13:27:00 +0200

    Noch nicht verwendeter Code auskommentiert

---
+++ Solution/DBSvcs/RnHsDBSvc.cs
@@ -0,0 +1,42 @@
+﻿//using Interfaces;
+//using Models.IDBSvc;
+//using Models.Common;
+//using DBSvcs.SqlConnectionExtensions;
+
+//namespace DBSvcs
+//{
+//    // All the code in this file is included in all platforms.
+//    public class RnHsDBSvc : DBSvc, IRnHsDBSvc
+//    {
+//        public RnHsDBSvc(ConnectionStringDBSettingsExt sqlConnectionSvc) : base(sqlConnectionSvc)
+//        {
+            
+//        }
+
+//        DBResult<RnH> IRnHsDBSvc.DeleteRnH(RnH RnH)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        DBResult<RnH> IRnHsDBSvc.InsertOnPosRnH(RnH RnH, int Pos)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        DBResult<RnH> IRnHsDBSvc.InsertRnH(RnH RnH)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        DBResult<RnH> IRnHsDBSvc.SaveRnH(RnH RnH)
+//        {
+//            throw new NotImplementedException();
+//        }
+
+//        //public DBResult<string> SaveTitle(string title)
+//        //{
+//        //    string cmd = $"UPDATE dbo.TitleTable SET Title = '{title}';";
+//        //    return _sqlConnection.Execute(cmd);
+//        //}
+//    }
+//}
---
+++ Solution/Interfaces/IRnHsDBSvc.cs
@@ -0,0 +1,17 @@
+﻿
+using Models.Common;
+using Models.IDBSvc;
+
+namespace Interfaces
+{
+    public interface IRnHsDBSvc : IDBSvc
+    {
+        DBResult<RnH> InsertRnH(RnH RnH);
+
+        DBResult<RnH> SaveRnH(RnH RnH);
+
+        DBResult<RnH> DeleteRnH(RnH RnH);
+
+        DBResult<RnH> InsertOnPosRnH(RnH RnH, int Pos);
+    }
+}
